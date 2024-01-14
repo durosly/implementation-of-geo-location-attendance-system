f1a10044af2b4f4dd4ddac5d5e636d63f2d38182
durosly <slyboydon1@gmail.com>
2024-01-14 17:43:29 +0100

    display notice

--- app/(auth)/user/page.tsx
+++ app/(auth)/user/page.tsx
@@ -3,9 +3,37 @@ import StatusWrapper from "./__components/status-wrapper";
 function UserDashboard() {
 	return (
 		<div className="text-center mt-10">
-			<div className="">
+			<div className=" mb-20">
 				<h2>Welcome, John Doe</h2>
 				<StatusWrapper />
+			</div>
+
+			<div
+				role="alert"
+				className="alert "
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					className="stroke-info shrink-0 w-6 h-6"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+					></path>
+				</svg>
+				<div>
+					<h3 className="font-bold">Note!</h3>
+					<div className="text-xs text-justify">
+						If you are within designated area and stil get a
+						designation error, your mobile location is not precise
+						and requires re-calibration. Visit google maps to help
+						you re-calibrate.
+					</div>
+				</div>
 			</div>
 		</div>
 	);
